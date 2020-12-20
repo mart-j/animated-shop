e58d89b797b153f6e42f39d55a14808c0e654194
mart-j <m.jukss@gmail.com>
2020-12-20 20:02:59 +0200

    deploying site

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  HashRouter,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Header from './components/header/Header';
@@ -7,15 +12,17 @@ import Header from './components/header/Header';
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <HashRouter basename="/">
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </HashRouter>
     </Router>
   );
 };
